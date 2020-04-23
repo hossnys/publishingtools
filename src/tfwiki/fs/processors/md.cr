1357c935aaeaa809195c400aa35d71aa910ed830
alichaddad <chaddada@greenitglobe.com>
2020-04-23 20:26:40 +0200

    add md fs processor

---
+++ src/tfwiki/fs/processors/md.cr
@@ -0,0 +1,14 @@
+module TfWiki
+  class MdProcessor # < Processor
+    def match(file_name)
+      return File.extname(file_name) == ".md"
+    end
+
+    def process(path_obj, child)
+      child_path = path_obj.join(child)
+      clean_child = child.downcase.gsub({" ": "_"})
+      new_path = path_obj.join(clean_child)
+      File.rename(child_path.to_s, new_path.to_s)
+    end
+  end
+end
