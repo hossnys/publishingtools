aa84a74e9458b490de56faf7a2c8200ba21c7f3b
xmonader <xmonader@gmail.com>
2020-04-27 12:19:47 +0200

    image->img fixer

--- src/tfwebserver/processors/imagedirrenamer.cr
+++ src/tfwebserver/processors/imagedirrenamer.cr
@@ -5,8 +5,9 @@ module TFWeb
     end
 
     def process(path_obj, child)
-      puts "[img->images]renaming dir images at #{path_obj.to_s} to #{path_obj.to_s}/img"
-      File.rename(path_obj.to_s, path_obj.join("img").to_s)
+      fullpath = path_obj.join(child)
+      puts "[img->images]renaming dir images at #{fullpath.to_s} to #{path_obj.to_s}/img"
+      File.rename(fullpath.to_s, path_obj.join("img").to_s)
     end
   end
 end
