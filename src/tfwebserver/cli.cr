53f41c84d3e5044722e27d66f686bbf7aa9d2ae5
xmonader <xmonader@gmail.com>
2020-05-13 10:42:08 +0200

    add version command

--- src/tfwebserver/cli.cr
+++ src/tfwebserver/cli.cr
@@ -4,12 +4,20 @@ require "toml"
 
 module TFWeb
   module CLI
+    BUILD_BRANCH   = {{ `git rev-parse --abbrev-ref HEAD`.chomp.stringify }}
+    BUILD_COMMIT   = {{ `git rev-parse HEAD`.chomp.stringify }}
+    BUILD_LAST_TAG = {{ `git describe --abbrev=0 --tags`.chomp.stringify }}
+    SHARDS_VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
+    VERSION = "tfweb #{BUILD_LAST_TAG} (#{BUILD_BRANCH})##{BUILD_COMMIT})"
+
     def self.main
       theconfigpath = ""
       OptionParser.parse! do |parser|
         parser.banner = "Usage: tfwiki [arguments]"
         parser.on("-c CONFIGPATH", "--config=CONFIGPATH", "TOML config file") { |configpath| theconfigpath = configpath }
         parser.on("-h", "--help", "Show this help") { puts parser }
+        parser.on("-v", "--version", "Version info of tfweb") { puts VERSION }
       end
 
       unless theconfigpath == ""
