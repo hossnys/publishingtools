1995c79368e87c01176f3983b6f1eb663f190391
xmonader <xmonader@gmail.com>
2020-05-17 13:20:13 +0200

    clone with a branch from the beginning instead of branchswitch

--- src/gittools/gitrepo.cr
+++ src/gittools/gitrepo.cr
@@ -110,7 +110,7 @@ module TFWeb
       rewritten_url = @url
       puts "cloning into #{@path}"
       unless Dir.exists?(@path)
-        `cd #{account_dir} && git clone #{rewritten_url} --depth=1 && cd #{@name} && git fetch`
+        `cd #{account_dir} && git clone -b #{@branch} #{rewritten_url} --depth=1 && cd #{@name} && git fetch`
       end
       if pull
         `cd #{guess_repo_dir} && git pull`
