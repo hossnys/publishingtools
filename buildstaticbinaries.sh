f551bfee8790f4cc39bfc6f19f57acb69334f1ea
xmonader <xmonader@gmail.com>
2020-06-16 10:52:51 +0200

    add linking to libssh2

--- buildstaticbinaries.sh
+++ buildstaticbinaries.sh
@@ -3,4 +3,4 @@
 
 #sudo docker run -it --name crystalalpine -h crystalalpine --privileged -v /opt/crystalalpine:/opt crystallang/crystal:0.34.0-alpine sh
 ## probably should add --release flag too
-shards build --static --link-flags "$(pkg-config libxml-2.0 --libs --static)"
+shards build --static --link-flags "$(pkg-config libxml-2.0 --libs --static)" --link-flags "$(pkg-config libssh2 --libs --static)"
